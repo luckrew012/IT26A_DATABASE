fd0303c086d8a2775199211e6746d2f464f7c571
luckrew0123 <mangkabongbenchpiet@gmail.com>
2026-04-08 17:14:33 +0800

    Initial commit

---
+++ src/Midterm/ConnectionDatabase.java
@@ -0,0 +1,27 @@
+
+package Midterm;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDatabase {
+    private static final String URL = "jdbc:mysql://localhost:3306/MangkabongDB";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() {
+        Connection conn = null;
+
+        try {
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Database Connected Successfully!");
+        } catch (SQLException e) {
+            System.out.println("Connection Failed!");
+            e.printStackTrace();
+        }
+
+        return conn;
+    }
+}
+
