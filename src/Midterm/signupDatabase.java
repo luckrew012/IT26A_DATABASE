a0b2470b45771251ec1cd845c052b17561a291ab
luckrew0123 <mangkabongbenchpiet@gmail.com>
2026-04-12 16:37:01 +0800

    designed a bit

--- src/Midterm/signupDatabase.java
+++ src/Midterm/signupDatabase.java
@@ -144,6 +144,9 @@ public class signupDatabase extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
+        loginDashboard dashh = new loginDashboard();
+        dashh.setVisible(true);
+        dispose();
     }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
